0c7f262df553855b15145f820a8ffcd5f3b5eeef
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-01-26 01:10:11 -0500

    feat: algorithm selection working

--- selection.go
+++ selection.go
@@ -6,26 +6,25 @@ import (
     
     tea "github.com/charmbracelet/bubbletea"
 )
+
 type model struct {
-    choices  []string           // items on the to-do list
+    algorithms  []string           // items on the to-do list
     cursor   int                // which to-do list item our cursor is pointing at
-    selected map[int]struct{}   // which to-do items are selected
+    selected int
 }
+
 func initialModel() model {
     return model{
-        // Our to-do list is a grocery list
-        choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-
-        // A map which indicates which choices are selected. We're using
-        // the map like a mathematical set. The keys refer to the indexes
-        // of the `choices` slice, above.
-        selected: make(map[int]struct{}),
+        algorithms: []string{"Bubble Sort", "Bogo Sort"},
+        selected: 0,
     }
 }
+
 func (m model) Init() tea.Cmd {
     // Just return `nil`, which means "no I/O right now, please."
     return nil
 }
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
 
@@ -47,19 +46,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
         // The "down" and "j" keys move the cursor down
         case "down", "j":
-            if m.cursor < len(m.choices)-1 {
+            if m.cursor < len(m.algorithms)-1 {
                 m.cursor++
             }
 
         // The "enter" key and the spacebar (a literal space) toggle
         // the selected state for the item that the cursor is pointing at.
         case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
+//            algo := m.algorithms[m.selected]
         }
     }
 
@@ -67,27 +61,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     // Note that we're not returning a command.
     return m, nil
 }
+
 func (m model) View() string {
     // The header
-    s := "What should we buy at the market?\n\n"
+    s := "Select an Algorithm\n\n"
 
-    // Iterate over our choices
-    for i, choice := range m.choices {
+    // Iterate over our algorithms
+    for i, algorithm := range m.algorithms {
 
-        // Is the cursor pointing at this choice?
+        // Is the cursor pointing at this algorithm?
         cursor := " " // no cursor
         if m.cursor == i {
             cursor = ">" // cursor!
         }
 
-        // Is this choice selected?
-        checked := " " // not selected
-        if _, ok := m.selected[i]; ok {
-            checked = "x" // selected!
-        }
-
         // Render the row
-        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+        s += fmt.Sprintf("%s %s\n", cursor, algorithm)
     }
 
     // The footer
